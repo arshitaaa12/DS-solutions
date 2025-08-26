b69f8cc12fef6a46b5008b6e0dab0487440ffe8e
aarshita_be24@thapar.edu <aarshita_be24@thapar.edu>
2025-08-27 02:12:44 +0530

    Added

---
+++ ass3_q2.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+#define MAX 100
+class Stack{
+    char arr[MAX];
+    int top;
+public:
+    Stack(){ 
+        top = -1; 
+    } 
+    void push(char c){ 
+        if(top == MAX- 1){ 
+            cout<<"Stack overflow"<<endl; 
+            return; 
+        } arr[++top] = c; 
+    } 
+    char pop(){ 
+        if(top == -1){ 
+            cout<<"Stack underflow"<<endl; 
+            return '\0'; 
+        } 
+        return arr[top--]; 
+    } 
+    bool isEmpty(){ 
+        return(top == -1); 
+    } 
+}; 
+int main(){ 
+    string str = "DataStructure"; 
+    Stack s; 
+    for(int i=0;i<str.length();i++){ 
+        s.push(str[i]); 
+    } 
+    string reversed = ""; 
+    while(!s.isEmpty()){ 
+        reversed += s.pop(); 
+    } 
+    cout<<"Original: "<<str<<endl; 
+    cout<<"Reversed: "<<reversed<<endl; 
+    return 0; 
+}
