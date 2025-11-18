a5c7c5adf54c84533ddc6d64db7a22902ec6f608
aarshita_be24@thapar.edu <aarshita_be24@thapar.edu>
2025-11-18 20:27:00 +0530

    Assignment 8 done

---
+++ assi8q5.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void heapify(int arr[], int n, int i){
+    int largest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if(left<n && arr[left]>arr[largest]){
+        largest = left;
+    }
+    if(right<n && arr[right]>arr[largest]){
+        largest = right;
+    }
+
+    if(largest != i){
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapsort(int arr[], int n, bool increasing = true){
+    for(int i=n/2 - 1;i>=0;i--){
+        heapify(arr, n, i);
+    }
+
+    for(int i=n-1;i>=0;i--){
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+
+    if(increasing){
+        for(int i=0;i<n/2;i++){
+            swap(arr[i], arr[n-i-1]);
+        }
+    }
+}
+
+int main(){
+    int arr[]  {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    heapsort(arr, n, true);
+
+    cout<<"Sorted array: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
